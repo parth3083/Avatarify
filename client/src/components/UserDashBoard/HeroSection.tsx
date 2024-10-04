67e8bdb13950a5a319e298ccc1f9ca6b934a19d7
parth3083 <parthbrajput30@gmail.com>
2024-10-05 00:46:20 +0530

    started the upload page

--- client/src/components/UserDashBoard/HeroSection.tsx
+++ client/src/components/UserDashBoard/HeroSection.tsx
@@ -1,12 +1,24 @@
+import Link from "next/link";
 import MaxWidth from "../MaxWidth";
 import CardContainer from "./CardContainer";
 
 function HeroSection() {
   return (
     <MaxWidth>
-      <h1 className="font-ala text-3xl lg:text-5xl capitalize font-bold mt-5">
-        my avatars
-      </h1>
+      <div className="w-full  h-20 flex items-center justify-between">
+        <div>
+          {" "}
+          <h1 className="font-ala text-3xl lg:text-5xl capitalize font-bold mt-5">
+            my avatars
+          </h1>
+        </div>
+        <Link
+          href={"/upload"}
+          className="px-2 py-2  rounded-md text-ala  text-white text-md lg:text-3xl font-medium cursor-pointer bg-[#2664EF]"
+        >
+          Genearte Avatar
+        </Link>
+      </div>
 
       <CardContainer />
     </MaxWidth>
